39fd31ae1ba0802ca562b208c5d8060db4da9a4c
Lis Setgasy <lisset10590@gmail.com>
2023-05-24 20:16:14 +0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// module.exports = nextConfig;
-
 /** @type {import("next").NextConfig} */
 module.exports = {
   experimental: {
@@ -12,5 +7,18 @@ module.exports = {
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/_next/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
   },
 };
