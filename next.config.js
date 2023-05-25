b2922e5e4df738b56b0bf581f8767ee2591a43ad
Lis Setgasy <lisset10590@gmail.com>
2023-05-25 22:26:30 +0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "beauty-salon-app.vercel.app/_next/(.*)",
+        source: "/_next/(.*)",
         headers: [
           {
             key: "Cache-Control",
