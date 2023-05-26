b4584029abecfdf1c83fc1825e7dbb7de2acaa9d
Lis Setgasy <lisset10590@gmail.com>
2023-05-26 20:33:53 +0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/_next/(.*)",
+        source: "/api/(.*)",
         headers: [
           {
             key: "Cache-Control",
