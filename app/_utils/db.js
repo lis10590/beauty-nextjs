32bcd35cfb6cf16c49a42ca04f39136a727c4822
lis10590 <lisset10590@gmail.com>
2024-05-08 20:23:18 +0300

    changed db connection

--- app/_utils/db.js
+++ app/_utils/db.js
@@ -1,15 +1,49 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the DATABASE_URL environment variable inside .env.local"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("Failed to connect to MongoDB", error);
+//   }
+// };
+
+// export default connectDB;
--- app/layout.js
+++ app/layout.js
@@ -10,10 +10,19 @@ import "react-toastify/dist/ReactToastify.css";
 import NavbarComp from "./_components/navbar";
 import store from "./_utils/store";
 import "react-loading-skeleton/dist/skeleton.css";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous"
+        />
+      </Head>
       <body>
         <Provider store={store}>
           <SessionProvider>
